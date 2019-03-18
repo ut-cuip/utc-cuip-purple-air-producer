32251b75fffaaef685d46340f667969f7e6e6495
Andrew Rodgers <acedrew@gmail.com>
2019-03-17 22:34:22 -0400

    Added nicenames to records

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ with open("config.yml", "r") as f:
 producer = KafkaProducer(bootstrap_servers=config["kafka"]["bootstrap_servers"])
 sensors = []
 mac_topic_map = {value["mac"]: value["topic"] for value in config["sensors"]}
+mac_nicename_map = {value["mac"]: value["nicename"] for value in config["sensors"]}
 
 
 def format_location(reading):
     reading["location"] = f"{reading['lat']}, {reading['lon']}"
     return reading
 
+def add_nicename(reading):
+    reading["nicename"] = mac_nicename_map[reading["SensorId"]]
+    return reading
 
 def format_timestamp(reading):
     reading["timestamp"] = (
@@ -34,6 +38,7 @@ def foo():
         reading = json.loads(request.get_data())
         reading = format_location(reading)
         reading = format_timestamp(reading)
+        reading = add_nicename(reading)
         sensorId = reading["SensorId"]
         producer.send(mac_topic_map[sensorId], json.dumps(reading).encode())
     except Exception as e:
